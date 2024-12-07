02a2cff041bf2b2f9af519f918f6d6001a6f028e
sidtuladhar <smt9598@nyu.edu>
2024-12-07 20:51:55 +0800

    added extras

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("mini.icons").setup()
-require("ibl").setup()
-require("mini.indentscope").setup({
-  symbol = "│",
-})
 require("lazygit")
